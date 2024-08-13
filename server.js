2454bf8a6aa9547d05eef4216dc7e15bc01f6628
NorthbyNature <info@northbynature.uk>
2024-08-13 01:04:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -85,5 +85,5 @@ app.post('/send-mail', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server running at https://www.northbynature.uk/`);
 });
