d60b7e735377bf86f50d95d3c01611aa11ee7589
NorthbyNature <info@northbynature.uk>
2025-06-30 14:51:58 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -524,14 +524,15 @@ membershipForm.addEventListener('submit', async function (e) {
 
 // Sticky header on scroll
 const header = document.querySelector('header');
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 0) {
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  }
-});
-
+if (header) {
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
+}
 document.addEventListener('DOMContentLoaded', () => {
   const vid = document.querySelector('.video-wrapper video');
   if (vid) {
